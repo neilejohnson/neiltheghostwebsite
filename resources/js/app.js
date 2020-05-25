7c7e18c2f106395627ef9145b98ca87663bccf16
unknown <seedownthesun@gmail.com>
2020-05-25 17:06:07 -0400

    developing header nav

---
+++ resources/js/app.js
@@ -0,0 +1,33 @@
+const image = document.querySelector("#header-logo")
+
+const navItems = [
+    {id: "#Nav-Comics", src: "resources/img/comics.png", alt:"comics-logo"},
+    {id: "#Nav-Illustration", src: "resources/img/illustration.png", alt:"illustration-logo"},
+    {id: "#Nav-Music", src: "resources/img/music.png", alt:"music-logo"},
+    {id: "#Nav-Store", src: "resources/img/store.png", alt:"store-logo"},
+    {id: "#Nav-About", src: "resources/img/about.png", alt:"about-logo"}
+];
+
+console.log(navItems[0].alt)
+
+//iterate through items list
+for (let item of navItems) {
+    //select current navitem
+    console.log(item);
+    const currentNav = document.querySelector(item.id);
+    console.log(currentNav)
+    currentNav.addEventListener("mouseover", function() {
+        image.src = item.src;
+        image.alt = item.alt;
+    });
+    currentNav.addEventListener("mouseout", function() {
+        image.src = "resources/img/home.png";
+        image.alt = "home-logo";
+    });
+};
+
+
+
+
+
+
