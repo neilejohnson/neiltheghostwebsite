2c8059e6cbe581dc5341d0002af69c2645e92635
unknown <seedownthesun@gmail.com>
2020-11-21 10:44:39 -0500

    expanding short-stories section

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,40 +2,9 @@ const mobileMenu = document.querySelector("#mobile-menu-icon");
 const mainNav = document.querySelector(".main-nav")
 
 const toggleNav = () => {
-    console.log('click');
     mainNav.classList.toggle('displayElement');
 }
 
 mobileMenu.addEventListener("click", toggleNav);
-
-// const image = document.querySelector("#header-logo")
-
-// const navItems = [
-//     {id: "#Nav-Comics", src: "resources/img/comics.png", alt:"comics-logo"},
-//     {id: "#Nav-Illustration", src: "resources/img/illustration.png", alt:"illustration-logo"},
-//     {id: "#Nav-Music", src: "resources/img/music.png", alt:"music-logo"},
-//     {id: "#Nav-Store", src: "resources/img/store.png", alt:"store-logo"},
-//     {id: "#Nav-About", src: "resources/img/about.png", alt:"about-logo"}
-// ];
-
-// //iterate through items list
-// for (let item of navItems) {
-//     //select current navitem
-//     console.log(item);
-//     const currentNav = document.querySelector(item.id);
-//     console.log(currentNav)
-//     currentNav.addEventListener("mouseover", function() {
-//         image.src = item.src;
-//         image.alt = item.alt;
-//     });
-//     currentNav.addEventListener("mouseout", function() {
-//         image.src = "resources/img/home.png";
-//         image.alt = "home-logo";
-//     });
-// };
-
-
-
-
 
 
